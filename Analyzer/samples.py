50467e0be43e9ab01f56cbdd836a62bb093a7c03
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2019-05-17 10:06:10 +0200

    update

--- Analyzer/samples.py
+++ Analyzer/samples.py
@@ -1,33 +1,54 @@
 import os
 import sys
 
-ntProd = "Flatfish-v20190514"
-ntPath = "maite.iihe.ac.be/pnfs/iihe/cms/store/user/kskovpen/tHGG/Ntuple/"
+from optparse import OptionParser
 
-fout = open("samples.xml","w+")
-fout.write('<data>\n')
+def main(argv = None):
+    
+    if argv == None:
+        argv = sys.argv[1:]
+        
+    usage = "usage: %prog [options]\n Script to produce a list of ntuples"
+        
+    parser = OptionParser(usage)
+    parser.add_option("-v","--version",default="Flatfish-v20190514",help="production version [default: %default]")
+    parser.add_option("-p","--path",default="maite.iihe.ac.be/pnfs/iihe/cms/store/user/kskovpen/tHGG/Ntuple/",help="storage path [default: %default]")
+    
+    (options, args) = parser.parse_args(sys.argv[1:])
+    
+    return options                                            
 
-dlist = os.popen('gfal-ls srm://'+ntPath+ntProd+'/').read().splitlines()
+if __name__ == '__main__':
+    
+    options = main()
 
-for i in dlist:
-    print i
-    isdata = str(int(i.find('DoubleEG') != -1))
-    fout.write("    <sample id=\""+i+"\" isdata=\""+isdata+"\">\n")
-    cpath = 'gfal-ls srm://'+ntPath+ntProd+'/'+i
-    d1 = os.popen(cpath).read().splitlines()
-    for i1 in d1:
-        d2 = os.popen(cpath+'/'+i1).read().splitlines()
-        if len(d2) != 1:
-            print 'Several runs found in', i 
-            exit
-        for i2 in d2:
-            d3 = os.popen(cpath+'/'+i1+'/'+i2).read().splitlines()
-            for i3 in d3:
-                d3 = os.popen(cpath+'/'+i1+'/'+i2+'/'+i3).read().splitlines()
-                for i4 in d3:
-                    fout.write("        <file>root://"+ntPath+ntProd+'/'+i+'/'+i1+'/'+i2+'/'+i3+'/'+i4+"</file>\n")
-    fout.write("    </sample>\n")
+    fout = open("samples.xml","w+")
+    fout.write('<data>\n')
 
-fout.write("</data>")
+    ntPath = options.path
+    ntProd = options.version
+    
+    dlist = os.popen('gfal-ls srm://'+ntPath+ntProd+'/').read().splitlines()
 
-fout.close()
+    for i in dlist:
+        print i
+        isdata = str(int(i.find('DoubleEG') != -1))
+        fout.write("    <sample id=\""+i+"\" isdata=\""+isdata+"\">\n")
+        cpath = 'gfal-ls srm://'+ntPath+ntProd+'/'+i
+        d1 = os.popen(cpath).read().splitlines()
+        for i1 in d1:
+            d2 = os.popen(cpath+'/'+i1).read().splitlines()
+            if len(d2) != 1:
+                print 'Several runs found in', i 
+                exit
+                for i2 in d2:
+                    d3 = os.popen(cpath+'/'+i1+'/'+i2).read().splitlines()
+                    for i3 in d3:
+                        d3 = os.popen(cpath+'/'+i1+'/'+i2+'/'+i3).read().splitlines()
+                        for i4 in d3:
+                            fout.write("        <file>root://"+ntPath+ntProd+'/'+i+'/'+i1+'/'+i2+'/'+i3+'/'+i4+"</file>\n")
+        fout.write("    </sample>\n")
+
+    fout.write("</data>")
+
+    fout.close()
--- README.md
+++ README.md
@@ -15,6 +15,14 @@ cd flashggPlugins/flashggAnalysisNtuplizer/test
 cmsRun flashggAnalysisNtuplizerWithSyst_cfg.py
 ```
 
+The list of produced ntuples can be found
+[here](https://github.com/kskovpen/tHGG/blob/master/Analyzer/samples.xml).
+It can be created with:
+
+```
+python samples.py --version=Flatfish-v20190514
+```
+
 ## Event selection
 
 Ntuples contain all events without any preselection applied. The
@@ -22,7 +30,7 @@ Ntuples contain all events without any preselection applied. The
 selects events by applying leptonic and hadronic selection criteria
 and produces output trees. 
 
-The analysis code does not depend on CMSSW and it can be installed with:
+The analysis code does not depend on CMSSW and it can be run with:
 
 ```
 git clone https://github.com/kskovpen/tHGG
