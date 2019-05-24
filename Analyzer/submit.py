11d664505860819b068a9b692ea6184a0f8d4da1
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2019-05-25 00:11:47 +0200

    fix a typo

--- Analyzer/submit.py
+++ Analyzer/submit.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
                     outlog = outname+'.log'
                     output = outname+'.root'
                     
-                    while (str(subprocess.check_output(['qsub','-N','Analyzer','-q',c.batchqueue,'-o',outlog,'-j','oe','job.sh','-l','walltime='+c.walltime,'-v','nmax='+options.nmax+',sample='+f+',xml='+fjobxml[jid+',output='+output+',dout='+home+',proxy='+c.proxydir+c.proxy+',arch='+c.arch])).find('Invalid credential') != -1):
+                    while (str(subprocess.check_output(['qsub','-N','Analyzer','-q',c.batchqueue,'-o',outlog,'-j','oe','job.sh','-l','walltime='+c.walltime,'-v','nmax='+options.nmax+',sample='+f+',xml='+fjobxml[jid]+',output='+output+',dout='+home+',proxy='+c.proxydir+c.proxy+',arch='+c.arch])).find('Invalid credential') != -1):
                         pass
                     
                     jid = jid + 1                    
