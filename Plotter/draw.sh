354b21472cbc9ed957294d73688fd42cc47355ec
sowuy <sophie.wuyckens@student.uclouvain.be>
2019-07-24 11:40:41 +0200

    change bash files

--- Plotter/draw.sh
+++ Plotter/draw.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
 #pn="_Muon"
+#pn="_Elec"
 pn="_All"
 #pn=""
 #sl="_SR2"
@@ -8,6 +9,24 @@ sl=""
 ch=leptonic
 #ch=hadronic
 
+#python draw.py \
+#--names=evNVtx${pn}${sl} \
+#--channel=${ch}
+
+#--names=evNVtx${pn}${sl},diPhoMass${pn}${sl},diPhoPt${pn}${sl},phoLeadpT${pn}${sl},\
+#phoLeadEta${pn}${sl},phoLeadIDMVA${pn}${sl},phoSubLeadpT${pn}${sl},phoSubLeadEta${pn}${sl},\
+#phoSubLeadIDMVA${pn}${sl},lepPt${pn}${sl},lepEta${pn}${sl},lepdeltaRPho${pn}${sl},\
+#jet1Pt${pn}${sl},jet2Pt${pn}${sl},jet1Eta${pn}${sl},jet2Eta${pn}${sl},jet1deltaRPho${pn}${sl},\
+#jet1deltaRLep${pn}${sl},jet2deltaRPho${pn}${sl},jet2deltaRLep${pn}${sl} \
+
 python draw.py \
---names=diPhoMass${pn}${sl} \
---channel=${ch} --factor=0.1
+--names=evNVtx${pn}${sl},diPhoMass${pn}${sl},diPhoPt${pn}${sl},phoLeadpT${pn}${sl},\
+phoLeadEta${pn}${sl},phoLeadIDMVA${pn}${sl},phoSubLeadpT${pn}${sl},phoSubLeadEta${pn}${sl},\
+phoSubLeadIDMVA${pn}${sl},lepPt${pn}${sl},lepEta${pn}${sl},\
+lepDeltaRLeadPho${pn}${sl},lepDeltaRSubLeadPho${pn}${sl},\
+jet1Pt${pn}${sl},jet2Pt${pn}${sl},jet1Eta${pn}${sl},jet2Eta${pn}${sl},\
+jet1deltaRLeadPho${pn}${sl},jet1deltaRSubLeadPho${pn}${sl},jet1deltaRLep${pn}${sl},jet2deltaRLeadPho${pn}${sl},jet2deltaRSubLeadPho${pn}${sl},\
+lepDeltaRHiggs${pn}${sl},jet1deltaRHiggs${pn}${sl},jet2deltaRHiggs${pn}${sl} \
+--channel=${ch} \
+--input=allHistos.root \
+--output=pics
--- Plotter/plot.sh
+++ Plotter/plot.sh
@@ -1,12 +1,30 @@
 #!/bin/env bash
+# Submission script
+#SBATCH --time=0-05:00:00 # days-hh:mm:ss
+#SBATCH --output=plot.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=5000 # megabytes
+#SBATCH --partition=Def
+#SBATCH --qos=normal
+#SBATCH --job-name=Plot
+#
 
+#names=evNVtx
 chan=leptonic
 #chan=hadronic
 #sl=SR1,SR2
-sl=''
-
-python plot.py \
---names=diPhoMass \
---channel=${chan} --blind=2 --selection=${sl}
+#sl='controlRegion,atLeastOneLepton,atLeastOneJet'
+sl=""
+#python plot.py \
+#--names=diPhoMass \
+#--channel=${chan} --blind=2 --selection=${sl}
 
 #--names=diPhoMass,diPhoMVA,phoLeadIDMVA,phoSubLeadIDMVA,metPt
+
+#python plot.py \
+#--names=$1 \
+#--names=diPhoMass,diPhoPt,phoLeadIDMVA,phoLeadpT,phoLeadEta,phoSubLeadIDMVA,phoSubLeadpT,phoSubLeadEta,lepPt,lepEta,lepDeltaRPho,evNVtx,jet1Pt,jet2Pt,jet1Eta,jet2Eta,jet1deltaRPho,jet1deltaRLep,jet2deltaRPho,jet2deltaRLep \
+#--channel=${chan} --selection=${sl}
+
+python plot_backup.py --names=$1 --channel=${chan} --selection=${sl} --output="output_"$1".root"
